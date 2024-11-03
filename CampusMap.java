0537c63cb43ae685dde5c7c7d8bdd7b68de809cc
mrehrman <mrehrman@smith.edu>
2024-11-03 11:11:29 -0500

    11/3

--- CampusMap.java
+++ CampusMap.java
@@ -45,6 +45,18 @@ public class CampusMap {
         myMap.addBuilding(new Building("Ford Hall", "100 Green Street Northampton, MA 01063", 4));
         myMap.addBuilding(new Building("Bass Hall", "4 Tyler Court Northampton, MA 01063", 4));
         System.out.println(myMap);
+        myMap.addBuilding(new Building("Dewey Hall", "4 Neilson Drive"));
+        myMap.addBuilding(new House ("Tyler", "164 Green Street", 4, true, false));
+        myMap.addBuilding(new House("101 Green Street"));//Morris House
+        myMap.addBuilding(new Library("Neilson Library", "7 Neilson Drive", 4, true));
+        myMap.addBuilding(new Library("122 Green Street", 2));//Josten Library
+        myMap.addBuilding(new Cafe("Compass Cafe", "7 Neilson Drive", 1, 50, 50, 50, 50));
+        myMap.addBuilding(new Cafe("100 Elm Street")); //Campus Center Cafe
+        myMap.addBuilding(new House("49 Elm Street", true, true));//Northrop House
+        myMap.addBuilding(new House("Comstock", "1 Mandelle Road", 4, true, true));
+        myMap.addBuilding(new House("Hubbard", "3 Green Street", 4, false, false));
+        System.out.println(myMap.toString());
+
     }
     
 }
